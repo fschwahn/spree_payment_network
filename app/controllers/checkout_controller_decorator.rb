ce893360a4eca036dc2b30e79a136ee021274d42
Fabian Schwahn <fabian.schwahn@gmail.com>
2011-11-04 10:59:21 +0100

    fixed error when coupon code is present

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -8,7 +8,7 @@ CheckoutController.class_eval do
     return unless params[:order][:payments_attributes]
     if params[:order][:coupon_code]
       @order.update_attributes(object_params)
-      @order.process_coupon_code
+      fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
     end
     load_order
     payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
